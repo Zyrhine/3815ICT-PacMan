0a2f36ddfdfe37b18139f9e9cab9f3f047020031
Zyrhine <contact@jeb.id.au>
2021-08-31 18:30:40 +1000

    Add ghost variants, ghost animations and basic AI

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,23 +1,148 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Tilemaps;
 
 public class Ghost : MonoBehaviour
 {
-    public GameObject target;
+    private enum State
+    {
+        Search,
+        Chase,
+        Return
+    }
+
+    private State curState = State.Search;
+    public float SearchSpeed = 1f;
+    public float ChaseSpeed = 2f;
+    private float chaseRange = 3f;
+    private MoveDirection _moveDirection = MoveDirection.Right;
+    private MoveDirection moveDirection
+    {
+        get => _moveDirection;
+        set
+        {
+            _moveDirection = value;
+            anim.SetInteger("Direction", (int)value);
+        }
+    }
+    private bool _isVulnerable = false;
+    private bool isVulnerable
+    {
+        get => _isVulnerable;
+        set
+        {
+            _isVulnerable = value;
+            anim.SetBool("isVulnerable", value);
+        }
+    }
+    private Animator anim;
+
+    private GameObject target;
     private NavMeshAgent agent;
     private bool stop;
+    public bool isSmart;
+
+    public GameObject homeWaypoint;
+    private Tilemap maze;
+    private Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
     {
+        target = GameObject.FindGameObjectWithTag("Player");
+        maze = GameObject.FindGameObjectWithTag("Maze").GetComponent<Tilemap>();
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        agent.speed = SearchSpeed;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        agent.SetDestination(target.transform.position);
+        destination = agent.destination;
+        switch (curState)
+        {
+            case State.Search:
+                UpdateSearch();
+                break;
+            case State.Chase:
+                UpdateChase();
+                break;
+            case State.Return:
+                UpdateReturn();
+                break;
+        }
+
+        UpdateDirection();
+    }
+
+    void UpdateDirection()
+    {
+        if (agent.velocity.x >= 1)
+        {
+            moveDirection = MoveDirection.Right;
+        }
+        if (agent.velocity.x <= -1)
+        {
+            moveDirection = MoveDirection.Left;
+        }
+        if (agent.velocity.y >= 1)
+        {
+            moveDirection = MoveDirection.Up;
+        }
+        if (agent.velocity.y <= -1)
+        {
+            moveDirection = MoveDirection.Down;
+        }
+    }
+
+    void UpdateSearch()
+    {
+        if (agent.remainingDistance < 1)
+        {
+            while (true)
+            {
+                Vector3Int cellPosition = new Vector3Int(Random.Range(1, 26), Random.Range(-29, -1), 0);
+                if (!maze.HasTile(cellPosition))
+                {
+                    var worldPosition = maze.CellToWorld(cellPosition);
+                    agent.SetDestination(worldPosition);
+                    break;
+                }
+            }
+        }
+
+        if (isSmart)
+        {
+            if (Vector3.Distance(gameObject.transform.position, target.transform.position) < chaseRange)
+            {
+                curState = State.Chase;
+                agent.speed = ChaseSpeed;
+            }
+        }
+    }
+
+    void UpdateChase()
+    {
+        if (Vector3.Distance(gameObject.transform.position, target.transform.position) < chaseRange)
+        {
+            agent.SetDestination(target.transform.position);
+        } else
+        {
+            curState = State.Search;
+            agent.speed = SearchSpeed;
+        }
+    }
+
+    void UpdateReturn()
+    {
+        agent.SetDestination(homeWaypoint.transform.position);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(destination, new Vector3(0.25f, 0.25f, 0.25f));
     }
 }
--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PacMan : MonoBehaviour
 {
-    public enum MoveDirection
-    {
-        Right,
-        Down,
-        Left,
-        Up
-    }
-
     public float MoveSpeed = 5f;
+    private MoveDirection queueDirection;
     private MoveDirection _moveDirection;
     private MoveDirection moveDirection
     {
@@ -36,46 +29,63 @@ public class PacMan : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Get directional input
         GetInput();
-        Vector2 dir = Vector2.zero;
 
-        switch(moveDirection)
+        // If the direction has been changed, change direction if PacMan is able to
+        if (moveDirection != queueDirection)
         {
-            case MoveDirection.Right:
-                dir = Vector2.right;
-                break;
-            case MoveDirection.Down:
-                dir = Vector2.down;
-                break;
-            case MoveDirection.Left:
-                dir = Vector2.left;
-                break;
-            case MoveDirection.Up:
-                dir = Vector2.up;
-                break;
+            if (!Physics2D.Raycast(rb.position, DirectionToVector(queueDirection), 0.16f))
+            {
+                moveDirection = queueDirection;
+            }
         }
 
+        // Move in the direction
+        Vector2 dir = DirectionToVector(moveDirection);
         Vector2 pos = rb.position + (dir * MoveSpeed * Time.fixedDeltaTime);
         rb.MovePosition(pos);
     }
 
+    // Gets the movement direction from input
     public void GetInput()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            moveDirection = MoveDirection.Right;
+            queueDirection = MoveDirection.Right;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            moveDirection = MoveDirection.Down;
+            queueDirection = MoveDirection.Down;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            moveDirection = MoveDirection.Left;
+            queueDirection = MoveDirection.Left;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            moveDirection = MoveDirection.Up;
+            queueDirection = MoveDirection.Up;
         }
     }
+
+    // Convert movement direction to Vector2
+    static Vector2 DirectionToVector(MoveDirection direction)
+    {
+        return direction switch
+        {
+            MoveDirection.Right => Vector2.right,
+            MoveDirection.Down => Vector2.down,
+            MoveDirection.Left => Vector2.left,
+            MoveDirection.Up => Vector2.up,
+            _ => Vector2.zero
+        };
+    }
+}
+
+public enum MoveDirection
+{
+    Right,
+    Down,
+    Left,
+    Up
 }
